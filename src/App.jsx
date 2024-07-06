ef4590bfcff4dad8b686cc6cf7bd7bd2f6968804
João Pedro Bagnara Boeing <joaopedroboeing688@gmail.com>
2024-07-05 22:03:44 -0300

    Alteracoes

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Cabecalho from "./components/Cabecalho";
+import Cartao from "./components/Cartao";
+
+const App =() => {
+return(
+  <div>
+    <Cabecalho/>
+
+    <main>
+
+      <h2>Página Inicial</h2>
+      <Cartao titulo="Exame de Gato" descricao="Check-up Completo de saúde para seu gato." 
+      imagem="img/gato.jpeg" />
+    </main>
+  </div>
+);
+};
