45487bdd1aa704e3111c4c77bc9fd7255131b3f5
João Pedro Bagnara Boeing <joaopedroboeing688@gmail.com>
2024-07-05 21:39:59 -0300

    Alteração

---
+++ src/components/Cabecalho.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Cabecalho = () => {
+return (
+
+    <header>
+        <h1>Bem-Vindo ao Pet House</h1>        
+        </header>
+
+);
+};
+
+export default Cabecalho
---
+++ src/components/Cartao.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Cartao = ({titulo, descricao, imagem}) => {
+return(
+<div className="cartao">
+    <img src={imagem} alt={titulo}/>
+<div className="conteudo-cartao">
+
+<h3>{titulo}</h3>
+<p>{descricao}</p>
+</div>
+</div>
+
+);
+};
